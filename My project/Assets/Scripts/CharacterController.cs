0dba774648b28456747324b302763116e07695ee
PowerOfDalaran <peklucz@gmail.com>
2022-08-24 23:11:24 +0200

    Adding rest of the assets
    I added rest of the assets to folders.

---
+++ My project/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    float movementSpeed = 3;
+    float rotationSpeed = 3;
+    float maxSpeed = 3;
+
+    Rigidbody2D rigidBody;
+
+    void Awake()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    public void Shoot()
+    {
+
+    }
+
+    public void Move()
+    {
+       
+    }
+
+    public void Rotate()
+    {
+
+    }
+}
